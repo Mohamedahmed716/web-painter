871987bbfbea2294efbff84bb935bdaa424e77ac
Mohamed Ahmed <es.Mohamed.nomier2023@alexu.edu.eg>
2025-11-22 06:56:35 +0200

    Added the project structure

---
+++ backend/src/main/java/com/painter/web_painter/WebPainterApplication.java
@@ -0,0 +1,13 @@
+package com.painter.web_painter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPainterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPainterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/painter/web_painter/WebPainterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painter.web_painter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPainterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
