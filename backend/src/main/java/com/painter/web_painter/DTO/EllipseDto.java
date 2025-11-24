fba22fe63a785a60f66d9b1766a209c205e7d507
Nour <nour.3bas752005@gmail.com>
2025-11-24 22:05:26 +0200

    my update

---
+++ backend/src/main/java/com/painter/web_painter/DTO/EllipseDto.java
@@ -0,0 +1,22 @@
+package com.painter.web_painter.DTO;
+
+public class EllipseDto extends ShapeDto {
+    private double radiusX;
+    private double radiusY;
+
+    public double getRadiusX() {
+        return radiusX;
+    }
+
+    public void setRadiusX(double radiusX) {
+        this.radiusX = radiusX;
+    }
+
+    public double getRadiusY() {
+        return radiusY;
+    }
+
+    public void setRadiusY(double radiusY) {
+        this.radiusY = radiusY;
+    }
+}
---
+++ backend/src/main/java/com/painter/web_painter/DTO/SquareDto.java
@@ -0,0 +1,13 @@
+package com.painter.web_painter.DTO;
+
+public class SquareDto extends ShapeDto{
+    double Sidelength;
+
+    public double getSidelength() {
+        return Sidelength;
+    }
+
+    public void setSidelength(double sidelength) {
+        Sidelength = sidelength;
+    }
+}
