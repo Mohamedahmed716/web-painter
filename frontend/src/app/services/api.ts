66e7e2e675ec4b8a4b22823d73ab69c48f6a24dc
abdelrhmanaboouf <abdelrhmanaboouf0@gmail.com>
2025-11-26 06:40:18 +0200

    dnjf

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -37,4 +37,55 @@ export class ApiService {
       .post<any[]>(`${this.baseUrl}/redo`, {})
       .pipe(map((data) => this.parsing.parse(data)));
   }
+  // api.ts
+select(x: number, y: number): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/select`, { x, y })
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+
+moveSelected(dx: number, dy: number): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/move`, { dx, dy })
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+
+resizeSelected(anchor: string, dx: number, dy: number): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/resize`, { anchor, dx, dy })
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+
+copySelected(): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/copy`, {})
+    .pipe(map(data => this.parsing.parse(data)));
+}
+pasteSelected(x: number, y: number): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/paste`, { x, y })
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+
+
+deleteSelected(): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/delete`, {})
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+startMove() {
+  return this.http.post<any[]>(`${this.baseUrl}/move/start`, {});
+}
+
+endMove() {
+  return this.http.post<any[]>(`${this.baseUrl}/move/end`, {});
+}
+updateColor(color: string): Observable<Shape[]> {
+  return this.http
+    .post<any[]>(`${this.baseUrl}/color`, { color })
+    .pipe(map((data) => this.parsing.parse(data)));
+}
+
+
+
 }
